a0add76329e4bfad9708b84096598f7da724793a
José Guilherme <guislima47@mail.com>
2017-07-06 20:01:45 -0300

    a

--- Trabalho1/src/main/RecursosList.java
+++ Trabalho1/src/main/RecursosList.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package main;
 
-/**
- *
- * @author JoséGuilherme
- */
-public class RecursosList {
+import java.util.ArrayList;
+
+public class RecursosList extends ArrayList{
     
 }
+
