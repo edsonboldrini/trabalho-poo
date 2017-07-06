4e0190a24991834fffa536f07f30d14f57195e74
José Guilherme <guislima47@mail.com>
2017-07-06 20:29:26 -0300

    atualizando

---
+++ Trabalho1/src/banco/AtvProcessoRecurso.java
@@ -0,0 +1,14 @@
+package banco;
+
+public class AtvProcessoRecurso extends Entidade {
+    private final int idRecurso;
+    private final int idAtividadeProcesso;
+    
+    public AtvProcessoRecurso(int idRecurso,int idAtividadeProcesso){
+        super("INSERT INTO ATVPROCESSO_RECURSO(ID_RECURSO,ID_ATVPROCESSO)"
+                + "VALUES("+ idRecurso + "," + idAtividadeProcesso);
+        
+        this.idAtividadeProcesso=idAtividadeProcesso;
+        this.idRecurso= idRecurso;
+    }  
+}
--- Trabalho1/src/banco/AtvRecurso.java
+++ Trabalho1/src/banco/AtvRecurso.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package banco;
 
-/**
- *
- * @author edson
- */
-public class AtvRecurso {
+public class AtvRecurso extends Entidade{
+    private int idCategoria;
+    private int idAtividade;
     
+    public AtvRecurso(int idCategoria, int idAtividade){
+        super("INSERT INTO ATIVIDADE_RECURSO(ID_CATEGORIA,ID_ATIVIDADE)"
+                + "VALUES("+idCategoria+","+idAtividade);
+    }
 }
